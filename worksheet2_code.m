cc33d555845642d46a07737bbef36c299862b7f8
davidaristoff <50423267+davidaristoff@users.noreply.github.com>
2022-09-22 11:21:53 -0600

    Delete worksheet2_code.m

--- worksheet2_code.m
+++
@@ -1,5 +0,0 @@
-ts = 0:1:12; vs = zeros(1,13);    %vectors of times and v values
-for t=1:12    %compute numerical solution using Euler's method
-    vs(t+1) = vs(t) + (49-vs(t))/5;
-end
-plot(ts,vs,'x'); vs    %plot solution and display solution values
